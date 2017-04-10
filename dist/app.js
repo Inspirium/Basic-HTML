1f103fcece3bc6e1dde953925a9570c79390868d
Stjepan-PC\Stjepan <sdrmic>
2017-04-10 22:33:06 +0200

    Input fields

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,8 @@ $(".button-collapse").sideNav();
 // Custom scrollbar init
 var el = document.querySelector('.custom-scrollbar');
 Ps.initialize(el);
+
+ // Material Select Initialization
+ $(document).ready(function() {
+    $('.mdb-select').material_select();
+  });
